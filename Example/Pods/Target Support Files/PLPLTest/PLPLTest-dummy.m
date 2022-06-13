1e7e30eea92b5006c7c89f4fe4bba7a69c6d816f
彭蕾 <penglei1@rongcloud.cn>
2022-06-13 10:36:57 +0800

    first commit

---
+++ Example/Pods/Target Support Files/PLPLTest/PLPLTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PLPLTest : NSObject
+@end
+@implementation PodsDummy_PLPLTest
+@end
---
+++ Example/Pods/Target Support Files/Pods-PLPLTest_Example/Pods-PLPLTest_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PLPLTest_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PLPLTest_Example
+@end
